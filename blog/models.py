051c3feb501af1e66a2380668ce90c83d21f9ae1
evgeni <darmanchev.evgeni@gmail.com>
2026-06-16 00:58:58 +0300

    fead: add model post

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'draft', 'Черновик'
+        PUBLISHED = 'published', 'Опубликован'
+        
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts",
+    )
+    body = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
