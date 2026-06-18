9a6ba08ac25313931bde3c400373f0e4070a9b78
evgeni <darmanchev.evgeni@gmail.com>
2026-06-19 01:56:25 +0300

    fix: rebuild html by using tailwind

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from .models import Post
 
 User = get_user_model()
@@ -55,3 +56,36 @@ class PostModelTest(TestCase):
             body='text post',
         )
         self.assertEqual(Post.objects.first(), post2)
+
+class PostViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='tester',
+                                             password='pass12345')
+        self.published = Post.objects.create(
+            title='Опубликованный', slug='published',
+            author=self.user, body='текст',
+            status=Post.Status.PUBLISHED,
+        )
+        self.draft = Post.objects.create(
+            title='Черновик', slug='draft',
+            author=self.user, body='текст',
+            status=Post.Status.DRAFT,
+        )
+
+    def test_list_returns_200(self):
+        response = self.client.get(reverse('blog:post_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_shows_only_published(self):
+        response = self.client.get(reverse('blog:post_list'))
+        self.assertContains(response, 'Опубликованный')
+        self.assertNotContains(response, 'Черновик')
+
+    def test_detail_returns_200(self):
+        response = self.client.get(self.published.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_draft_returns_404(self):
+        response = self.client.get(reverse('blog:post_detail', args=['draft']))
+        self.assertEqual(response.status_code, 404)
