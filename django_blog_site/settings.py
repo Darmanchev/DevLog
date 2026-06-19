36126e08f4851add3319f92d13e4b6f4970004a8
evgeni <darmanchev.evgeni@gmail.com>
2026-06-19 10:46:56 +0300

    feat: add auth page and logic

--- django_blog_site/settings.py
+++ django_blog_site/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+LOGIN_REDIRECT_URL = 'blog:post_list'
+LOGOUT_REDIRECT_URL = 'blog:post_list'
+LOGIN_URL = 'login'
