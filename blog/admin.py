04daf76e949b8386d03119377c348a5af335ca83
evgeni <darmanchev.evgeni@gmail.com>
2026-06-21 12:46:57 +0300

    feat: add slug autogenerate, and add test for it

--- blog/admin.py
+++ blog/admin.py
@@ -7,4 +7,3 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'status', 'created_at']
     list_filter = ['status', 'created_at']
     search_fields = ['title', 'body']
-    prepopulated_fields = {'slug': ('title',)}
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('new/', views.PostCreateView.as_view(), name='post_create'),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
