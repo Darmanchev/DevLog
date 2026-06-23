bb43c1a1caba40f60f2c451f1b75c160c3c8fbe2
evgeni <darmanchev.evgeni@gmail.com>
2026-06-23 21:51:12 +0300

    feat: add comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -7,3 +7,9 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'status', 'created_at']
     list_filter = ['status', 'created_at']
     search_fields = ['title', 'body']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created_at']
+    list_filter = ['created_at']
